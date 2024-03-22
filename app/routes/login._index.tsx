d9456b97c570641e86ce2fac6a13293bcd5e488a
scossar <scossar3@gmail.com>
2024-03-21 21:11:22 -0700

    commit before fixing type error

--- app/routes/login._index.tsx
+++ app/routes/login._index.tsx
@@ -1,6 +1,8 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { Form, Link, useActionData } from "@remix-run/react";
 
+import LoginForm from "~/components/LoginForm";
+
 import { createUserSession, login } from "~/services/session.server";
 import { badRequest } from "~/services/request.server";
 
@@ -57,77 +59,20 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   }
 };
 
-export default function LoginForm() {
+export default function Login() {
   const actionData = useActionData<typeof action>();
-  console.log(
-    `actionData.fieldErrors: ${JSON.stringify(
-      actionData?.fieldErrors,
-      null,
-      2
-    )}`
-  );
+  const fields = actionData?.fields;
+  const fieldErrors = actionData?.fieldErrors;
+  const formError = actionData?.formError;
 
   return (
     <div>
       <h1 className="text-2xl">Log In</h1>
-      <Form
-        className="flex flex-col max-w-80 border border-slate-400 p-3"
-        method="post"
-      >
-        <label htmlFor="username-or-email">Username or Email</label>
-        <input
-          className="border border-slate-600 px-1"
-          type="text"
-          id="username-or-email"
-          name="usernameOrEmail"
-          defaultValue={actionData?.fields?.usernameOrEmail}
-          aria-invalid={Boolean(actionData?.fieldErrors?.usernameOrEmail)}
-          aria-errormessage={
-            actionData?.fieldErrors?.usernameOrEmail
-              ? "Username or Email error"
-              : ""
-          }
-        />
-        {actionData?.fieldErrors?.usernameOrEmail && (
-          <p className="text-sm text-red-600">
-            {actionData.fieldErrors.usernameOrEmail}
-          </p>
-        )}
-        <label htmlFor="password">Password</label>
-        <input
-          className="border border-slate-600 px-1"
-          type="password"
-          id="password"
-          name="password"
-          defaultValue={actionData?.fields?.password}
-          aria-invalid={Boolean(actionData?.fieldErrors?.password)}
-          aria-errormessage={
-            actionData?.fieldErrors?.password ? "Password error" : ""
-          }
-        />
-        {actionData?.fieldErrors?.password && (
-          <p className="text-sm text-red-600">
-            {actionData.fieldErrors.password}
-          </p>
-        )}
-        {actionData?.formError && (
-          <div className="border border-red-500 p-2 my-2 rounded-sm text-sm">
-            {actionData.formError}
-          </div>
-        )}
-
-        <div className="mt-4">
-          <button className="border border-slate-600 w-16" type="submit">
-            Login
-          </button>
-        </div>
-      </Form>
-      <Link
-        className="text-sky-700 text-sm hover:underline"
-        to="/login/register"
-      >
-        Register an account
-      </Link>
+      <LoginForm
+        fields={fields}
+        fieldErrors={fieldErrors}
+        formError={formError}
+      />
     </div>
   );
 }
